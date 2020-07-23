2594d018aaed2ab5cceed3cd212ebe8a2b68360e
Amee Li <ameejli@gmail.com>
2020-07-22 18:03:19 -0700

    Add navbar

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,17 +10,39 @@ class Counters extends Component {
       { id: 4, value: 0 },
     ],
   };
+
   handleDelete = (counterId) => {
     const counters = this.state.counters.filter((c) => c.id !== counterId);
     this.setState({ counters });
   };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
+        <button className="btn btn-primary m-2" onClick={this.handleReset}>
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
-            key={counter.id}
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            key={counter.id}
             counter={counter}
           />
         ))}
